35ec3d5e910afe166490d2cf57202e62b0678f5b
Prince Kumar <213578506+prinsipekumar@users.noreply.github.com>
2026-09-02 12:51:54 +0530

    Add "Roman to Integer" Solution File

---
+++ RomanToInteger.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class RomanToInteger {
+    public int romanToInt(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+
+        int result = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int value = map.get(s.charAt(i));
+            if (i + 1 < s.length() && value < map.get(s.charAt(i + 1))) {
+                result -= value;
+            } else {
+                result += value;
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        RomanToInteger sol = new RomanToInteger();
+        System.out.println(sol.romanToInt("III"));      //3
+        System.out.println(sol.romanToInt("LVIII"));    //58
+        System.out.println(sol.romanToInt("MCMXCIV"));  //1994
+    }
+}
