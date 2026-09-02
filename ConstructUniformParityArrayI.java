89baace63accc5d38d2bd808c858a43f341293d3
Prince Kumar <213578506+prinsipekumar@users.noreply.github.com>
2026-09-02 12:56:49 +0530

    Upload "Construct Uniform Parity Array I"

---
+++ ConstructUniformParityArrayI.java
@@ -0,0 +1,11 @@
+public class ConstructUniformParityArrayI {
+    public boolean uniformArray(int[] nums1) {
+        return true;
+    }
+
+    public static void main(String[] args) {
+        ConstructUniformParityArrayI sol = new ConstructUniformParityArrayI();
+        int[] nums1 = {2, 3};
+        System.out.println(sol.uniformArray(nums1)); //true
+    }
+}
