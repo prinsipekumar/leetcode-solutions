e397a4db1b3dd48852adddb316c8fa409d550aba
Prince Kumar <213578506+prinsipekumar@users.noreply.github.com>
2026-09-06 22:34:32 +0530

    Plus One

---
+++ PlusOne.java
@@ -0,0 +1,27 @@
+public class PlusOne {
+    public static int[] plusOne(int[] digits) {
+        int n = digits.length;
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        int[] result = new int[n + 1];
+        result[0] = 1;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] digits = {9, 9, 9};
+        int[] result = plusOne(digits);
+
+        System.out.print("Result: ");
+        for (int d : result) {
+            System.out.print(d);
+        } //1000
+    }
+}
