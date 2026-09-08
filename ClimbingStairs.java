4e3318fc948d2e1caf635aafb0f9d5208fb96ed7
Prince Kumar <213578506+prinsipekumar@users.noreply.github.com>
2026-09-08 11:31:51 +0530

    Climbing Stairs

---
+++ ClimbingStairs.java
@@ -0,0 +1,22 @@
+public class ClimbingStairs {
+    public int climbStairs(int n) {
+        if (n <= 2) return n;
+
+        int prev1 = 1;
+        int prev2 = 2;
+
+        for (int i = 3; i <= n; i++) {
+            int current = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = current;
+        }
+
+        return prev2;
+    }
+
+    public static void main(String[] args) {
+        ClimbingStairs sol = new ClimbingStairs();
+        int n = 5;
+        System.out.println("Ways to climb " + n + " stairs: " + sol.climbStairs(n)); //8
+    }
+}
