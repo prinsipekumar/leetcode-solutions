0bf43d7f0f52c7aa29f834db285673351f4acfe8
Prince Kumar <213578506+prinsipekumar@users.noreply.github.com>
2026-09-15 22:45:42 +0530

    Array Prototype Last

---
+++ ArrayPrototypeLast.js
@@ -0,0 +1,10 @@
+Array.prototype.last = function () {
+  if (this.length === 0) return -1;
+  return this[this.length - 1];
+};
+
+const arr1 = [1, 2, 3];
+console.log(arr1.last()); //3
+
+const arr2 = [];
+console.log(arr2.last()); //-1
