c6f048db36d1433d722935473fff69aeb9997125
Prince Kumar <213578506+prinsipekumar@users.noreply.github.com>
2026-09-21 23:57:55 +0530

    Generate Fibonacci Sequence

---
+++ FibGenerator.js
@@ -0,0 +1,14 @@
+var fibGenerator = function* () {
+  let a = 0,
+    b = 1;
+  while (true) {
+    yield a;
+    [a, b] = [b, a + b];
+  }
+};
+
+const gen = fibGenerator();
+console.log(gen.next().value); //0
+console.log(gen.next().value); //1
+console.log(gen.next().value); //1
+console.log(gen.next().value); //2
